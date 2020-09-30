a902fc7c4b2df42fb6e6421d37c5b3d2af3365ad
Bruno Marini <bruno.marini1@gmail.com>
2020-09-30 17:15:42 -0300

    [^] Query: language support

--- src/utils/query.js
+++ src/utils/query.js
@@ -1,9 +1,9 @@
-module.exports = (cursor) => {
+module.exports = (cursor, language) => {
   const after = cursor ? `, after: "${cursor}"` : '';
 
   return `
   {
-    search(type: REPOSITORY, query: "stars:>100", first: 5${after}) {
+    search(type: REPOSITORY, query: "stars:>100 language:${language}", first: 5${after}) {
       repositoryCount
       pageInfo {
         endCursor
